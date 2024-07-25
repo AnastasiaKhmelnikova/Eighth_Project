dcd6e7efa139f66faac860962055f66b31b7a79f
Anastasia <nastya.khmelnikova@gmail.com>
2024-07-25 18:51:16 +0300

    commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name; // поле для имени
+    String passport;
+    String patronymic;
+    String phone;
+    String surname;
+    boolean subscription;
+    FormDate birthday;
+}
